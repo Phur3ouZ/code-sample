e87a4eac0a8a298c608578572a409527bbda8341
Bradley Wong <phur3ouz+git@hotmail.com>
2021-10-14 20:04:54 +1300

    - Added relationship between `items` and `prices`

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends ModelWithHistory
@@ -9,4 +10,12 @@ class Item extends ModelWithHistory
     use SoftDeletes;
 
     protected $table = 'items';
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function Prices(): HasMany
+    {
+        return $this->hasMany(Price::class, 'item_id', 'id');
+    }
 }
