b9be19250f8cdac23fc5ce751f3266b58366dc63
Bradley Wong <phur3ouz+git@hotmail.com>
2021-10-14 17:59:27 +1300

    - Added route for `items`, via /api/items - Updated controller for `items`    - Can perform all standard CRUD operations

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,48 +2,123 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ItemResource;
 use App\Models\Item;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 
 class ItemController extends Controller
 {
     /**
-     * Get all items
+     * Retrieve all records from the `items` table
      *
      * @return \Illuminate\Http\JsonResponse
      */
     public function index(): JsonResponse
     {
-        return response()->json();
+        $items = Item::query()->get();
+
+        $resource = ItemResource::collection($items);
+        return $resource->response();
     }
 
     /**
+     * Add a new record into the `items` table
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(): JsonResponse
+    {
+        $rules = [
+            'barcode' => ['required'],
+            'name' => ['string', 'nullable'],
+            'description' => ['string', 'nullable'],
+        ];
+
+        try {
+            $payload = $this->validate(request(), $rules);
+        } catch (ValidationException $e) {
+            return response()->json($e->getMessage(), 400);
+        }
+
+        try {
+            $item = new Item();
+            $item->updateWithTransaction($payload);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 400);
+        }
+
+        $resource = new ItemResource($item);
+        return $resource->response();
+    }
+
+    /**
+     * Retrieve a specific record from the `items` table by id
+     *
      * @param  int  $id
      * @return \Illuminate\Http\JsonResponse
      */
     public function show(int $id): JsonResponse
     {
-        $item = Item::query()->findOrFail($id);
+        try {
+            $item = Item::query()->findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json($e->getMessage(), 400);
+        }
 
-        return response()->json();
+        $resource = new ItemResource($item);
+        return $resource->response();
     }
 
-    /**
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function store(): JsonResponse
-    {
-        return response()->json();
-    }
 
     /**
+     * Update a specific record from the `items` table by id
+     *
      * @param  int  $id
      * @return \Illuminate\Http\JsonResponse
      */
     public function update(int $id): JsonResponse
     {
-        $item = Item::query()->findOrFail($id);
+        $rules = [
+            'barcode' => ['required'],
+            'name' => ['string', 'nullable'],
+            'description' => ['string', 'nullable'],
+        ];
 
-        return response()->json();
+        try {
+            $payload = $this->validate(request(), $rules);
+        } catch (ValidationException $e) {
+            return response()->json($e->getMessage(), 400);
+        }
+
+        try {
+            $item = Item::query()->findOrFail($id);
+            $item->updateWithTransaction($payload);
+        } catch (ModelNotFoundException | \Exception $e) {
+            return response()->json($e->getMessage(), 400);
+        }
+
+        $resource = new ItemResource($item);
+        return $resource->response();
+    }
+
+    /**
+     * Soft-delete a specific record from the `items` table by id
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(int $id): JsonResponse
+    {
+        try {
+            $item = Item::query()->findOrFail($id);
+        } catch (ModelNotFoundException | \Exception $e) {
+            return response()->json($e->getMessage(), 400);
+        }
+
+        $item->delete();
+
+        return response()->json('Deletion successful');
     }
 }
