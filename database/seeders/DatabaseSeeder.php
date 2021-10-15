318d10b40896cfd23867bf287d0c898bf7108600
Bradley Wong <phur3ouz+git@hotmail.com>
2021-10-15 14:17:22 +1300

    - Added seeder to populate some data into the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Http\Repositories\ItemRepository;
+use App\Interfaces\IRepository;
+use App\Models\Currency;
+use App\Models\Item;
+use App\Models\Price;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * @var \App\Http\Repositories\ItemRepository
+     */
+    private IRepository $repository;
+
+    public function __construct(ItemRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     /**
      * Seed the application's database.
      *
@@ -13,6 +29,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Remove existing data
+        Item::query()->truncate();
+        Price::query()->truncate();
+        Currency::query()->truncate();
+
+        // Add data using repository
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 25; $i++) {
+            $this->repository->update(new Item(), [
+                $this->repository::FIELD_BARCODE => $faker->ean13(),
+                $this->repository::FIELD_NAME => $faker->name,
+                $this->repository::FIELD_DESCRIPTION => $faker->sentence,
+                $this->repository::FIELD_PRICE => [
+                    [
+                        'amount' => $faker->numberBetween(100, 2000),
+                        'currency' => config('currency.default'),
+                    ]
+                ]
+            ]);
+        }
+
+        // Add data using command
+        Artisan::call('currency:load');
     }
 }
